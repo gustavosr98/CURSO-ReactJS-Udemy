736839d73e7b51800d0bb7a7cbf5ac8cff470bbb
Gustavo Sánchez <gsanchez@conectium.com>
2019-02-18 16:53:56 -0400

    Termine CreateExpense Component

--- 12_React_Redux/03_Mi_Intento/src/components/CreateExpense.js
+++ 12_React_Redux/03_Mi_Intento/src/components/CreateExpense.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import ExpenseForm from './ExpenseForm'
+import { addExpense } from '../actions/expenses'
 
-export default () => (
+const CreateExpense = (props) => (
     <div>
         <h1>Create Expense</h1>
-        <ExpenseForm />
+        <ExpenseForm 
+            onSubmit={ (expense) => {
+                props.dispatch( addExpense(expense) )
+                // ME REDIRECCIONA AL PATH QUE QUIERA
+                props.history.push('/')
+            }}
+        />
     </div>
 ) 
+
+export default connect()(CreateExpense)
--- 12_React_Redux/03_Mi_Intento/src/components/ExpenseForm.js
+++ 12_React_Redux/03_Mi_Intento/src/components/ExpenseForm.js
@@ -6,17 +6,18 @@ import 'react-dates/lib/css/_datepicker.css'
 export default class ExpenseForm extends React.Component {
     // LOCAL STATE PARA SEGUIR LOS CAMBIOS ANTES DE ENVIARLOS EN SUBMIT 
     state = {
-        descripcion: "",
+        description: "",
         note: "",
         amount: "",
         createdAt: moment(),
-        calendarFocused : false
+        calendarFocused : false,
+        error: ''
     }
 
     onDescriptionChange = (e) => {
-        const descripcion = e.target.value
+        const description = e.target.value
         this.setState( () => ({
-            descripcion
+            description
         }))
     }
 
@@ -30,24 +31,41 @@ export default class ExpenseForm extends React.Component {
     onAmountChange = (e) => {
         const amount = e.target.value
         /* REGULAR EXPRESIONS - REGEX101.COM*/
-        if (amount.match( /^\d*(\.\d{0,2})?$/ ))
+        if (!amount || amount.match( /^\d{1,}(\.\d{0,2})?$/ ))
             this.setState( () => ({
                 amount
             }))
     }
 
     onDateChange = (createdAt) => {
-        this.setState( () => ({createdAt}) )
+        if (createdAt)
+            this.setState( () => ({createdAt}) )
     }
 
     onFocusChange = ({focused}) => {
         this.setState( () => ({calendarFocused:focused}) )
     }
 
+    onSubmit = (e) => {
+        e.preventDefault()
+        if (!this.state.amount || !this.state.description) {
+            this.setState(()=>({error:"Please provide description and amount!."}))
+        } else {
+            this.setState(()=>({error:""}))
+            this.props.onSubmit({
+                description: this.state.description,
+                note: this.state.note,
+                amount: parseFloat(this.state.amount, 10) *100,
+                createdAt: this.state.createdAt.valueOf()
+            })
+        }
+    }
+
     render(){
         return (
             <div>
-                <form>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.onSubmit}>
                     <input 
                         type="text"
                         placeholder="Insert descripcion"
