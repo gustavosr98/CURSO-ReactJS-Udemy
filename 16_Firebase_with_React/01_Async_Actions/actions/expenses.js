3b996d4c5e170c0f8799e86ad78a5ea75f8f747e
Gustavo Sánchez <gsanchez@conectium.com>
2019-02-21 15:16:37 -0400

    Firebase with Redux

---
+++ 16_Firebase_with_React/01_Async_Actions/actions/expenses.js
@@ -0,0 +1,31 @@
+import uuid from 'uuid';
+import database from '../firebase/firebase';
+
+// ADD_EXPENSE
+export const addExpense = (expense) => ({
+  type: 'ADD_EXPENSE',
+  expense
+}); 
+
+export const startAddExpense = (expenseData = {}) => {
+  // ESTO RETORNA UNA F(X) - FUNCION
+  return (dispatch) => {
+    // DESTRUCTURING Y VALORES POR DEFECTO
+    const {
+      description = '',
+      note = '',
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+
+    const expense = { description, note, amount, createdAt };
+
+    // refArray.push( elemento ).then ( despacha la accion al redux store )
+    database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+};
