00c9d7c05b7dd999478b6244ede8c84312f69d50
Gustavo Sánchez <gsanchez@conectium.com>
2019-02-21 10:57:56 -0400

    Remove y Update Firebase

---
+++ 15_Firebase_101/04_Update_db/firebase.js
@@ -0,0 +1,39 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyAapA5P8gWC83aHK26xgDDMzIBC4dw914w",
+  authDomain: "expensify-a13e6.firebaseapp.com",
+  databaseURL: "https://expensify-a13e6.firebaseio.com",
+  projectId: "expensify-a13e6",
+  storageBucket: "expensify-a13e6.appspot.com",
+  messagingSenderId: "124728096317"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+database.ref().set({
+  name: 'Andrew Mead',
+  age: 26,
+  stressLevel: 6,
+  job: {
+    title: 'Software developer',
+    company: 'Google'
+  },
+  location: {
+    city: 'Philadelphia',
+    country: 'United States'
+  }
+}).then(() => {
+  console.log('Data is saved!');
+}).catch((e) => {
+  console.log('This failed.', e);
+});
+
+// HAY MEJORES EJEMPLOS DE UPDATE() EN MI CUADERNO _REACTJS
+database.ref().update({
+  stressLevel: 9,
+  'job/company': 'Amazon',
+  'location/city': 'Seattle'
+});
